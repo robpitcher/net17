c40e7e19b839c7f3edc6684bd0a2eafdae89a9a8
rdp870 <13648061+robpitcher@users.noreply.github.com>
2022-12-06 16:08:52 -0500

    testing pipeline trigger

--- scripts/thing1.ps1
+++ scripts/thing1.ps1
@@ -1,14 +1,14 @@
-$output1 = get-content -Path .\outputs\output1.txt
+# $output1 = get-content -Path .\outputs\output1.txt
 
-$currentRevision = ($output1 | Select-String "RevisionVer=\d*").ToString().Split('=')[1]
+# $currentRevision = ($output1 | Select-String "RevisionVer=\d*").ToString().Split('=')[1]
 
-$currentRevision = ($currentRevision / 1)
+# $currentRevision = ($currentRevision / 1)
 
-$currentRevision ++
+# $currentRevision ++
 
-$newRevision = $currentRevision
+# $newRevision = $currentRevision
 
-$output1 -replace "RevisionVer=\d*","RevisionVer=$newRevision" | Set-Content -Path .\outputs\output1.txt
+# $output1 -replace "RevisionVer=\d*","RevisionVer=$newRevision" | Set-Content -Path .\outputs\output1.txt
 
 
 # Added a feature
