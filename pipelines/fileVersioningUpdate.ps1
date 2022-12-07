8fdaaad75ac4cbbc9fed82302a9bf1c2796b2bab
rdp870 <13648061+robpitcher@users.noreply.github.com>
2022-12-07 10:16:29 -0500

    added comments

--- pipelines/fileVersioningUpdate.ps1
+++ pipelines/fileVersioningUpdate.ps1
@@ -3,27 +3,29 @@ $stgAccount = Get-AzStorageAccount -ResourceGroupName storagerg -Name ropistg
 $stgCtx = $stgaccount.Context
 $stgTable = Get-AzStorageTable -name versions -Context $stgCtx
 $cloudTable = $stgTable.CloudTable
-
-# Get the rows
 $partitionKey1 = "partition1"
 
+# Get the row data
 $tableRow = Get-AzTableRow -Table $cloudTable -PartitionKey $partitionKey1 -RowKey ${env:rowKey}
 
-$output1 = get-content -Path ${env:repoPath}\outputs\output1.txt
-
-$currentRevision = ($output1 | Select-String "RevisionVer=\d*").ToString().Split('=')[1]
-
+# Get current version info from row data
 $versionInfo = $tableRow.version
+
+# Isolate and convert revision from string to int
 $currentRevision = ($versionInfo.Split('.')[-1] / 1)
 
+# Increment revision number
 $currentRevision ++
 
-# Set incremented revision
+# Get project file contents w/ existing revision number
+$output1 = get-content -Path ${env:repoPath}\outputs\output1.txt
+
+# Replace existing revision number with updated revision 
 $output1 -replace "RevisionVer=\d*","RevisionVer=$currentRevision" | Set-Content -Path ${env:repoPath}\outputs\output1.txt
 
-# Create updated version number
+# Create updated full version number
 $newVersion = $versionInfo.Split('.')[0] + "." + $versionInfo.Split('.')[1] + "." + $currentRevision
 
+# Update the table row with the new version number
 $tableRow.version = $newVersion
-
 $tableRow | Update-AzTableRow -Table $cloudTable
